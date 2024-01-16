06756ce80ce494cc271d2c08f9963ab7d60eea14
wizhou <5711693+wizhou@users.noreply.github.com>
2024-01-16 12:27:08 +0100

    Update index.php
    Add directories for blueprints

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ Kirby::plugin('wizhou/kirby-coeur-de-fer', [
     'blueprints' => [
         'blocks/rule' => __DIR__ . '/blueprints/rule.yml',
         'blocks/lead' => __DIR__ . '/blueprints/lead.yml',
-        'blocks/heading' => __DIR__ . '/blueprints/heading.yml'
+        'blocks/heading' => __DIR__ . '/blueprints/heading.yml',
+        'fields/margins' => __DIR__ . '/blueprints/fields/margins.yml',
+        'fields/text-align' => __DIR__ . '/blueprints/fields/text-align.yml'
     ]
 ]);
