27093e6bd096f12ff78285c6f959b3a93ce74b18
wizhou <5711693+wizhou@users.noreply.github.com>
2024-01-16 17:03:06 +0100

    Implement text-transform into snippets
    Add text-transform style into the blocks snippets

--- snippets/heading.php
+++ snippets/heading.php
@@ -1,10 +1,11 @@
 <?php
 $level = $block->level()->or('h2');
 $textAlign = $block->text_align()->or('left');
+$textTransform = $block->text_transform()->or('unset');
 ?>
 <<?= $level; ?>
     class="headings headings-<?= $block->margins(); ?>"
-    style="text-align: <?= $textAlign; ?>;"
+    style="text-align: <?= $textAlign; ?>; text-transform: <?= $textTransform; ?>"
 >
     <?= $block->text(); ?>
 </<?= $level; ?>>
--- snippets/lead.php
+++ snippets/lead.php
@@ -3,11 +3,12 @@ $size = $block->size()->or('lead');
 $class = $block->class();
 $id = $block->id();
 $textAlign = $block->text_align()->or('left');
+$textTransform = $block->text_transform()->or('unset');
 ?>
 <div
   class="headword <?= $size; ?> <?= $class; ?>"
   <?= Html::attr(['id' => $id], null, ' ') ?>
-  style="text-align: <?= $textAlign; ?>;"
+  style="text-align: <?= $textAlign; ?>; text-transform: <?= $textTransform; ?>"
 >
   <?= $block->text()->kirbytextinline(); ?>
 </div>
