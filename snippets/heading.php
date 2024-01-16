fb3ee66144a0e31ab9778614938be83dc72169f0
wizhou <5711693+wizhou@users.noreply.github.com>
2024-01-16 12:13:40 +0100

    Update heading.php
    Refacotring for clarity and add the style param for text align

--- snippets/heading.php
+++ snippets/heading.php
@@ -1,5 +1,10 @@
-<?php $level = $block->level()->or('h2'); ?>
-
-<<?= $level; ?> class="headings headings-<?= $block->margins(); ?>">
+<?php
+$level = $block->level()->or('h2');
+$textAlign = $block->text_align()->or('left');
+?>
+<<?= $level; ?>
+    class="headings headings-<?= $block->margins(); ?>"
+    style="text-align: <?= $textAlign; ?>;"
+>
     <?= $block->text(); ?>
-  </<?= $level; ?>>
+</<?= $level; ?>>
